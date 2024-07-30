965205b9b28f81dd55e11dfc3a2a1cc32deee699
jt989073 <thompsonjames0120@gmail.com>
2024-07-30 16:06:57 -0500

    added options object

--- backend/db/migrations/20240730201944-create-show.js
+++ backend/db/migrations/20240730201944-create-show.js
@@ -1,4 +1,10 @@
 'use strict';
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -37,9 +43,10 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Shows');
+    options.tableName = 'Shows'
+    await queryInterface.dropTable(options);
   }
 };
